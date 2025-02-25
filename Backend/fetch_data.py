fde1515810118355e9c9c67bbd44fb76291e4a8d
Xxenia1 <157653332+Xxenia1@users.noreply.github.com>
2025-02-25 17:15:38 -0600

    create data folder

--- Backend/fetch_data.py
+++ Backend/fetch_data.py
@@ -1,1 +1,30 @@
+# fetch fire mode data
+
+# %% python visualize
 import ee
+import json
+
+ee.Initialize(project='canvas-radio-444702-k2')
+
+# %% fetch real-time fire data, using VIIRS
+noaa_viirs = ee.ImageCollection('NASA/LANCE/NOAA20_VIIRS/C2') \
+            .filterDate(ee.Date.today().advance(-12, 'hour'), ee.Date.today()) \
+            .select(['FireMask'])
+
+## convert imagery to vector point
+fire_vectors = noaa_viirs.reduceToVectors(
+    reducer=ee.Reducer.countEvery(),
+    geometryType='centroid',  ## convert to point
+    scale=375,  # 375m resolution
+    maxPixels=1e13
+)
+
+## convert to geojson
+fire_geojson = fire_vectors.getInfo()
+
+output_file = "Data/real_fire.json" ## save 
+with open(output_file, "w") as f:
+    json.dump(fire_geojson, f, indent=4)
+print('Done')
+
+# %%
