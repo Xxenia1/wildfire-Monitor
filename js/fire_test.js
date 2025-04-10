69fa6df646ec1d4658a011c4de2410ce9da4763d
Xxenia1 <157653332+Xxenia1@users.noreply.github.com>
2025-04-10 18:57:19 -0500

    1111

--- js/fire_test.js
+++ js/fire_test.js
@@ -5,30 +5,55 @@ let realtimeLayer = null;
 let historicalLayer = null;
 
 // Add Real-time Fire Data Layer
-export function addRealtimeLayer(map) {
-  fetch('https://storage.googleapis.com/wildfire-monitor-data/RT_fire_data/fires_tile_4_20250408.geojson.geojson')
-    .then(res => res.json())
-    .then(data => {
-      const realtimeLayer = L.geoJSON(data, {
-        pointToLayer: (feature, latlng) => L.circleMarker(latlng, {
-          radius: 4,
-          fillColor: 'red',
-          color: '#800000',
-          weight: 0.5,
-          fillOpacity: 0.8
-        }),
-        onEachFeature: (feature, layer) => {
-          const count = feature.properties.count ?? 'N/A';
-          layer.bindPopup(`🔥 Real-time Fire<br>Count: ${count}`);
-        }
-      }).addTo(map);
+export async function addRealtimeLayer(map) {
+  try{
+    // get all files in RT_fire_data folder
+    const response = await fetch(
+      "https://storage.googleapis.com/storage/v1/b/wildfire-monitor-data/o?prefix=RT_fire_data/&alt=json"
+    );
+    const json = await response.json();
 
-      map.fitBounds(realtimeLayer.getBounds());
-      console.log("✅ Real-time fire data loaded");
-    })
-    .catch(err => {
-      console.error(" Failed to load real-time fire data:", err);
-    });
+    //filter geojson files only
+    const geojsonFiles = json.items
+      .filter(item => item.name.endsWith(".geojson"))
+      .sort((a, b) => new Date(b.updated) - new Date(a.updated)); // newest first
+
+    if (geojsonFiles.length === 0) {
+        throw new Error("No GeoJSON files found in RT_fire_data.");
+    }
+
+    const latestFile = geojsonFiles[0].name;
+    const fileUrl = `https://storage.googleapis.com/wildfire-monitor-data/${latestFile}`;
+
+    console.log(" Latest real-time fire data:", fileUrl);
+
+    // Step 3: Fetch the latest geojson and visualize
+    const geojsonRes = await fetch(fileUrl);
+    const data = await geojsonRes.json();
+    // check attribute
+    console.log("Sample feature properties:", data.features[0].properties);
+
+    realtimeLayer = L.geoJSON(data, {
+      pointToLayer: (feature, latlng) => L.circleMarker(latlng, {
+        radius: 4,
+        fillColor: 'red',
+        color: '#800000',
+        weight: 0.5,
+        fillOpacity: 0.8
+      }),
+      onEachFeature: (feature, layer) => {
+        const count = feature.properties.count ?? 'N/A';
+        layer.bindPopup(` Real-time Fire<br>Count: ${count}`);
+      }
+    }).addTo(map);
+
+    map.fitBounds(realtimeLayer.getBounds());
+    console.log(" Real-time fire layer added");
+
+  } catch (err) {
+    console.error(" Failed to load real-time fire data:", err);
+  }
+  
 }
 
 
